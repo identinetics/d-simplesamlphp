000ec76260b3c2e6a4500df8dfa528651ca4970f
r2h2 <rainer@hoerbe.at>
2016-11-28 17:45:05 +0100

    docfix regexp

--- install/scripts/init_config_ssp.sh
+++ install/scripts/init_config_ssp.sh
@@ -14,10 +14,17 @@ cp -pr /opt/default/postfix /etc/
 echo "initializing attributemap/, config/ and metadata/ with default data"
 cp -pr $SSP_ROOT/attributemap-templates/* $SSP_ROOT/attributemap/
 cp -pr $SSP_ROOT/metadata-templates/* $SSP_ROOT/metadata/
+mkdir -p /var/simplesaml/metadata/metarefresh-federation
+chown 33:33 /var/simplesaml/metadata/metarefresh-federation
 cp -p  $SSP_ROOT/config-templates/* $SSP_ROOT/config/
+
+# set default logging to file
 perl -i -pe "s{^(\s*)'logging.handler'\s+=> 'syslog'}{$1'logging.handler' => 'file'}" $SSP_ROOT/config/config.php
+# set location for federated metadata
 perl -i -pe "s{^(\s*)array('type' => 'flatfile')}{$1array('type' => 'serialize', 'directory' => 'metadata\/metarefresh-federation'),}" $SSP_ROOT/config/config.php
-perl -i -pe "s{^(\s*)'store.type'\s+=> 'phpsession',}{$1'store.type => 'sql',}" $SSP_ROOT/config/config.php
+# set store-type to sql
+perl -i -pe "s{^(\s*)'store.type'\s+=> 'phpsession',}{$1'store.type' => 'sql',}" $SSP_ROOT/config/config.php
+# set default session db
 perl -i -pe "s{'sqlite:/path/to/sqlitedatabase.sq3'}{'sqlite:/tmp/sqlitedatabase.sq3'}" $SSP_ROOT/config/config.php
 
 
